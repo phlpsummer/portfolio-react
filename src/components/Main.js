37a579510c27fe13a6ad00111202df443aff1c9e
phplpsummer <sh052100@naver.com>
2022-01-16 23:19:24 +0900

    메인인터렉션 레이아웃

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,16 @@
+import Panels from './Panels.js';
+import Btns from './Btns.js';
+import { useRef } from 'react';
+
 function Main(){
+    let frame = useRef(null);
     return (
         <figure id="visual">
-            <h2>Main</h2>
+            <section ref={frame}>
+                <Panels />
+            </section>
+
+            <Btns frame={frame} />
         </figure>
     )
 }
---
+++ src/components/Panels.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+const arr = ['Bali','Taipei','Macao','Osaka'];
+const len = arr.length;
+const deg = 360/len;
+const path = process.env.PUBLIC_URL;
+
+function Panels(){
+    let [names, setNames] = useState(arr);
+    return (
+        <>
+            {
+                names.map((name,index)=>{
+                    let style = {transform: `rotate(${deg*index}deg) translateY(-100vh)`};
+                    let imgSrc = `${path}/img/${name}.jpg`;
+                    return (
+                        <article key={index} style={style}>
+                            <div className="inner">
+                                <div className="pic">
+                                    <img src={imgSrc} />
+                                </div>
+                                <h2>{name}</h2>
+                            </div>
+                        </article>
+                    )
+                })
+            }
+        </>
+    )
+}
+export default Panels;
