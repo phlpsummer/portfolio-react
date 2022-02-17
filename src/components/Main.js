84a90e2c4f5c62cbd61c75f36ba3ae4ce968933b
phplpsummer <sh052100@naver.com>
2022-02-17 21:56:13 +0900

    메인 section 추가(마켓,띠배너,타임딜)

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,22 +5,39 @@ import { useRef } from 'react';
 function Main(){
     let frame = useRef(null);
     return (
-        <figure id="visual">
-            <section ref={frame}>
-                <Boards />
+        <>
+            <figure id="visual">
+                <section ref={frame}>
+                    <Boards />
+                </section>
+
+                <div className="deco_left">
+                    <p>Explore</p>
+                    <span>we can go anywhere.</span>
+                </div>
+                
+                <div className="deco_right">
+                    <p>kr</p>
+                </div>
+
+                <Btn frame={frame} />
+            </figure>
+            <section className='market'>
+                <div className="inner">
+
+                </div>
             </section>
+            <section className='banner'>
+                <div className="inner">
 
-            <div className="deco_left">
-                <p>Explore</p>
-                <span>we can go anywhere.</span>
-            </div>
-            
-            <div className="deco_right">
-                <p>kr</p>
-            </div>
-
-            <Btn frame={frame} />
-        </figure>
+                </div>
+            </section>
+            <section className='timedeal'>
+                <div className="inner">
+                    
+                </div>
+            </section>
+        </>
     )
 }
 export default Main;
